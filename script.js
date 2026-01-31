1e84e880321950285777fdc010afca2ddd9e07e2
valkdz <valkdzx@gmail.com>
2026-01-31 22:49:03 +0200

    Update script.js

--- script.js
+++ script.js
@@ -70,7 +70,6 @@ async function searchVideo() {
         btn.className = "option-btn resolution-btn" + (i === 0 ? " active" : "");
         btn.dataset.formatId = f.format_id;
         btn.textContent = `${f.height}p`;
-        if (f.filesize) btn.textContent += ` (${(f.filesize / 1024 / 1024).toFixed(1)} MB)`;
         resolutionOptions.appendChild(btn);
       });
     }
